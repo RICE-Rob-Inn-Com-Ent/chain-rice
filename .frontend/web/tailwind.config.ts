53f456d9721cde1156884b9d40dfc2cc467ec840
mrDinkelman185 <imenimene69@gmail.com>
2025-10-27 22:07:59 +0100

    chore: auto-commit at 2025-10-27 22:07:59 [branch: v.0.1.0]

--- .frontend/web/tailwind.config.ts
+++ .frontend/web/tailwind.config.ts
@@ -2,12 +2,9 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    './src/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './layouts/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    './index.tsx',
     './index.html',
+    './**/*.{js,ts,jsx,tsx,html}',
   ],
 
   plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
--- .frontend/web/vite.config.ts
+++ .frontend/web/vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import postcssPresetEnv from 'postcss-preset-env';
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import postcssPresetEnv from "postcss-preset-env";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',
-  publicDir: 'public',
+  root: ".",
+  publicDir: "public",
   server: {
     port: 3001,
     strictPort: false,
@@ -15,7 +15,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': '/home/mrDinkelman/rice-mono/.frontend/web',
+      "@": "/home/mrDinkelman/rice-mono/.frontend/web",
     },
   },
   css: {
@@ -24,10 +24,10 @@ export default defineConfig({
         postcssPresetEnv({
           stage: 1,
           features: {
-            'nesting-rules': true,
-            'custom-properties': true,
-            'custom-media-queries': true,
-            'media-query-ranges': true,
+            "nesting-rules": true,
+            "custom-properties": true,
+            "custom-media-queries": true,
+            "media-query-ranges": true,
           },
         }),
         tailwindcss(),
@@ -36,6 +36,6 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: 'dist',
+    outDir: "dist",
   },
 });
