aee08873916950146a861b8c41f12e78004a9bfb
mrDinkelman185 <imenimene69@gmail.com>
2025-10-27 21:27:27 +0100

    chore: auto-commit at 2025-10-27 21:27:27 [branch: v.0.1.0]

--- .frontend/web/index.tsx
+++ .frontend/web/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import Header from "./lib/molecules/Header";
+import Header from "./molecules/Header";
 import "./index.css";
 
 function App() {
