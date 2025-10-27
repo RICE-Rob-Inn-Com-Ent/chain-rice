7840ffaa32f7e08650b2f5c0816cff28bbc718e5
mrDinkelman185 <imenimene69@gmail.com>
2025-10-27 22:15:30 +0100

    chore: auto-commit at 2025-10-27 22:15:30 [branch: v.0.1.0]

--- .frontend/web/layouts/index.ts
+++ .frontend/web/layouts/index.ts
@@ -4,13 +4,4 @@
  */
 
 export { default as Header } from "./Header";
-export { default as Navbar } from "./TopBar";
 export { default as Footer } from "./Footer";
-export { default as ContactForm } from "./ContactForm";
-export { default as TechStackGrid } from "./TechStackGrid";
-export { default as PricingCalculator } from "./PricingCalculator";
-export { default as ChatWidget } from "./ChatWidget";
-export { GodCard } from "./GodCard";
-export { GodAwakeningProgress } from "./GodAwakeningProgress";
-export { GodsPanel } from "./GodsPanel";
-export { LoRaTrainingPanel } from "./LoRaTrainingPanel";
