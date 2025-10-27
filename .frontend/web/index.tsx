5d3e562b290573a76e487df7b202137e21c593b1
mrDinkelman185 <imenimene69@gmail.com>
2025-10-27 21:03:57 +0100

    chore: auto-commit at 2025-10-27 21:03:57 [branch: v.0.1.0]

--- .frontend/web/index.tsx
+++ .frontend/web/index.tsx
@@ -10,3 +10,12 @@ function App() {
   return (
     <Header>
       <HomePage />
+      <AboutPage />
+      <ServicesPage />
+      <TeamPage />
+      <ContactPage />
+    </Header>
+  );
+}
+
+export default App;
