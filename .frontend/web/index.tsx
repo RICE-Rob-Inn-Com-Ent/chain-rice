6273f506ee8f8150073a2288ac3b70d4f8e21bc2
mrDinkelman185 <imenimene69@gmail.com>
2025-10-27 21:04:57 +0100

    chore: auto-commit at 2025-10-27 21:04:57 [branch: v.0.1.0]

--- .frontend/web/index.tsx
+++ .frontend/web/index.tsx
@@ -1,19 +1,10 @@
 import React from "react";
 import { Header } from "@rice/ui-kit/lib";
-import HomePage from "./app/pages/Home";
-import AboutPage from "./app/pages/About";
-import ServicesPage from "./app/pages/Service";
-import TeamPage from "./app/pages/TeamPage";
-import ContactPage from "./app/pages/Contact";
+
 
 function App() {
   return (
     <Header>
-      <HomePage />
-      <AboutPage />
-      <ServicesPage />
-      <TeamPage />
-      <ContactPage />
     </Header>
   );
 }
