8c1359049af9c4d330ed8649c44c2d6fa162a4be
mrDinkelman185 <imenimene69@gmail.com>
2025-10-28 22:12:39 +0100

    chore: auto-commit at 2025-10-28 22:12:39 [branch: v.0.1.0]

--- .frontend/web/app/index.ts
+++ .frontend/web/app/index.ts
@@ -2,5 +2,7 @@ export { Layout } from "./Layout";
 export { Dashboard } from "./Dashboard";
 export { Models } from "./Models";
 export { LoRaTraining } from "./LoRaTraining";
+export { GodTraining } from "./GodTraining";
 export { Prices } from "./Prices";
 export { ComponentLibrary } from "./ComponentLibrary";
+export * from "./training";
